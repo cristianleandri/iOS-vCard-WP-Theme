515fce6aafcf243591b568e6fa29d90fa41760ce
Dries Vints <dries.vints@gmail.com>
2012-09-29 16:22:26 +0200

    Basic site setup with content and styles
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<!--[if lt IE 7]>    <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>       <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>       <html lang="en" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <!-- Info -->
+    <title>Dries Vints</title>
+    <meta name="description" content="I'm a web developer working at Volta Web in Antwerp. I'm working mostly on CMS systems and developing web applications. I also recently started working with iOS. Check out CaMo App, a Campaign Monitor client that I’m building.">
+    
+    <!-- Viewport Fix -->
+    <meta name="viewport" content="width=device-width">
+
+    <!-- CSS Styles -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<div id="wrapper">
+    <div id="wrapper-cell">
+        <article id="content">
+            <header>
+                <img class="avatar" src="http://www.gravatar.com/avatar/<?php echo md5('dries.vints@gmail.com'); ?>?s=95" alt="">
+                <h1>Dries Vints</h1>
+                <h2>Web &amp; Software Developer</h2>
+            </header>
+            <div class="clear"></div>
+            <p>
+                I&rsquo;m a <strong>web developer</strong> working at <a href="http://www.volta.be">Volta Web</a> in Antwerp. I&rsquo;m working mostly on <strong>CMS systems</strong> and <strong>web applications</strong>.<br>
+                I also recently started working with <strong>iOS</strong>. Check out <strong>@</strong><a href="http://twitter.com/camoapp">CaMoapp</a>, a <a href="http://www.campaignmonitor.com">Campaign Monitor</a> client that I’m building.
+            </p>
+        </article>
+    </div>
+</div>
+
+<!-- Google Analytics -->
+<script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+
+</body>
+</html>
